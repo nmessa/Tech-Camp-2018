a67169a4f2293af2518115c9cb47678ab9f535d8
nmessa <nmessa@sau16.org>
2018-07-25 19:49:02 -0400

    Add files via upload

--- Tech Camp Arduino Code/rangeFinder/rangeFinder.ino
+++ Tech Camp Arduino Code/rangeFinder/rangeFinder.ino
@@ -1,7 +1,7 @@
 #include <NewPing.h>
 
-#define TRIGGER_PIN  12           // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     11             // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define TRIGGER_PIN  11           // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     10             // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200    // Maximum distance we want to ping for (in centimeters). 
 // Maximum sensor distance is rated at 400-500cm.
 
